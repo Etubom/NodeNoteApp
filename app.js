410191fa044c4a549f479f2e9070897609374b1a
etubom <way2young4u@hotmail.com>
2018-10-10 23:20:38 +0100

    Added process object and used argv cmd lists

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const fs = require("fs");
 const _ = require("lodash");
 
 const notes = require("./notes.js");
+
+let command = process.argv[2]; //node internal object + value holding list of everything provided on the cmd line
+console.log("Command: ", command);
+
+if (command === "add") {
+  console.log("Adding new note");
+} else if (command === "list") {
+  console.log("Listing all notes");
+} else if (command === "read") {
+  console.log("Fetching note");
+} else if (command === "remove") {
+  console.log("Removing note");
+} else {
+  console.log("command not recognised");
+}

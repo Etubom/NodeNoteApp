b6164b63b9a3074f8ae0f9ad0e54ae240cdad1db
etubom <way2young4u@hotmail.com>
2018-10-10 20:38:43 +0100

    removed all commented code

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 console.log("Starting app.js");
 const fs = require("fs");
-const os = require("os");
+
 const _ = require("lodash");
 const notes = require("./notes.js");
-
-let filteredArray = _.uniq(["Edem", 1, "Edem", 1, 2, 3, 2]);
-console.log(filteredArray);
-// console.log(_.isString(true));
-// console.log(_.isString("Edem"));
-
-// let user = os.userInfo();
-// console.log(`Result will be:`, notes.add(5, 5));
-
-// fs.appendFile(
-//   "greetings.txt",
-//   `Hello ${user.username}!, you are ${notes.age} years old.`,
-//   function(err) {
-//     if (err) {
-//       console.log("Unable to write to file");
-//     }
-//   }
-// );
